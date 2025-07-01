07eaea04e5031a79138f11cc1e4c6b8dd653fa1b
PepMargarit <pepmgdevelop@gmail.com>
2025-07-01 06:00:33 +0200

    Clases base de Paciente y Evaluacion

---
+++ Models/Evaluacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitodietCalc.Models
+{
+    class Evaluacion
+    {
+        public int Id { get; set; }
+        public required string PacienteId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double PesoKg { get; set; }
+        public double AlturaCm { get; set; }
+        public double IMC { get; set; }
+        public double GrasaCorporal { get; set; } = 0;
+        public double MasaMuscular { get; set; } = 0;
+        
+        public enum ActividadFisica
+        {
+            Sedentario,
+            Moderado,
+            Activo,
+            MuyActivo
+
+        }
+
+        private double CalcularIMC()
+        {
+            return PesoKg / (AlturaCm * AlturaCm);
+        }
+
+    }
+}
---
+++ Models/Paciente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitodietCalc.Models
+{
+    class Paciente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; } = string.Empty; // Default value to avoid null
+        public string Apellido2 { get; set; } = string.Empty; // Default value to avoid null
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Sexo { get; set; }        
+
+        public Paciente(string nombre, string apellido1, string apellido2, string email, string telefone, DateTime fechaNascimento, string sexo)
+        {
+            Nombre = nombre;
+            Apellido1 = apellido1;
+            Apellido2 = apellido2;
+            Email = email;
+            Telefono = telefone;
+            FechaNacimiento = fechaNascimento;
+            Sexo = sexo;            
+            
+        }
+
+        
+    }
+}
